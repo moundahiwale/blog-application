524e336c43f3b4c855868e5496e18503bc53d2cd
moundahiwale <moundahiwale@yahoo.com>
2020-07-27 10:03:54 +0100

    Add post detail component test using stubs

--- src/app/components/posts/post-detail/post-detail/post-detail.component.spec.ts
+++ src/app/components/posts/post-detail/post-detail/post-detail.component.spec.ts
@@ -1,19 +1,50 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
 import { PostDetailComponent } from './post-detail.component';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, PartialObserver } from 'rxjs';
+import { CommentsService } from 'src/app/services/comments/comments.service';
+import Post from 'src/app/models/post';
 
-describe('PostDetailComponent', () => {
+const mockPost = {
+  id: 1,
+  title: 'Blog post #1',
+  author: 'Melissa Manges',
+  publish_date: '2016-02-23',
+  slug: 'blog-post-1',
+  description: 'Utroque denique invenire et has.',
+  content:
+    '<p>Utroque denique invenire et has. Cum case definitiones no, est dicit placerat verterem ne.</p> <p>In ius nonumy perfecto adipiscing, ad est cibo iisque aliquid, dicit civibus eum ei. Cum animal suscipit at, utamur utroque appareat sed ex.</p>',
+};
+
+const mockPostWrapper = {
+  post: mockPost,
+};
+
+const activatedRouteStub = {
+  data: Observable.create((observer: PartialObserver<any>) => {
+    observer.next(mockPostWrapper);
+  }),
+};
+
+const commentsServiceStub = {
+  fetchComments: Observable.create((observer: PartialObserver<Post[]>) => {
+    observer.next([mockPost]);
+  }),
+};
+
+fdescribe('PostDetailComponent', () => {
   let component: PostDetailComponent;
   let fixture: ComponentFixture<PostDetailComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PostDetailComponent ]
-    })
-    .compileComponents();
+      declarations: [PostDetailComponent],
+      imports: [],
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRouteStub },
+        { provide: CommentsService, useValue: commentsServiceStub },
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -24,5 +55,9 @@ describe('PostDetailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should have post from mock post`, () => {
+    expect(component.post).toBe(mockPost);
   });
 });
